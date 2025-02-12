999ff45f2087e2b29ee8e33c6529d228f06803d3
Koen Kruk <Koen.Kruk@ICloud.com>
2025-02-12 10:22:38 -0500

    add config.py

---
+++ game.py
@@ -0,0 +1,35 @@
+import pygame
+import sys
+import config
+
+def init_game():
+    pygame.init()
+    screen = pygame.display.set_mode((config.WINDOW_RESOLUTION[0], config.WINDOW_RESOLUTION[1]))
+    pygame.display.set_caption("Pygame Template")
+    return screen
+
+def handle_events():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                return False
+    return True
+
+def main():
+    screen = init_game()
+    clock = pygame.time.Clock()
+    running = True
+    while running:
+        running = handle_events()
+        screen.fill(config.WHITE)
+        pygame.display.flip()
+
+        clock.tick(config.FPS)
+
+    pygame.quit()
+    sys.exit()
+
+if __name__ == "__main__":
+    main()
