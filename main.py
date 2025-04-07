d1e8205c081d1a2cc8334fe8ae5213d40c222a69
nebulimity <78868475+nebulimity@users.noreply.github.com>
2025-04-07 10:26:08 -0400

    update and rename game.py to main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import config
 
 def init_game():
     pygame.init()
+    
     screen = pygame.display.set_mode((config.WINDOW_RESOLUTION[0], config.WINDOW_RESOLUTION[1]))
     pygame.display.set_caption("Pygame Template")
     return screen
@@ -20,12 +21,17 @@ def handle_events():
 def main():
     screen = init_game()
     clock = pygame.time.Clock()
+
+    
+
     running = True
     while running:
         running = handle_events()
         screen.fill(config.WHITE)
-        pygame.display.flip()
 
+        
+        
+        pygame.display.flip()
         clock.tick(config.FPS)
 
     pygame.quit()
